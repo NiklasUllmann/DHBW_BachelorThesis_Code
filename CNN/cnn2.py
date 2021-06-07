d61e691c36f178529a6e854494f8f0546222e845
NiklasUllmann <niklas.ullmann@web.de>
2021-06-07 11:34:55 +0200

    Update cnn2.py

--- CNN/cnn2.py
+++ CNN/cnn2.py
@@ -19,7 +19,7 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 6, 5)
         self.pool = nn.MaxPool2d(5, 5)
-        self.fc1 = nn.Linear(6*11*11, 256)
+        self.fc1 = nn.Linear(726, 256)
         self.fc2 = nn.Linear(256, 64)
         self.fc1 = nn.Linear(64, 10)
 
@@ -28,7 +28,9 @@ class CNN(nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         print(x.shape)
         x = x.view(-1, 6*11*11)
+        print(x.shape)
         x = F.relu(self.fc1(x))
+        print(x.shape)
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
